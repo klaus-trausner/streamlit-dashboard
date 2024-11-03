b395dda3369849c730ef254c608f590c1890554f
Klaus Trausner <57252686+klaus-trausner@users.noreply.github.com>
2024-11-03 15:13:33 +0000

    pages

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,18 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+# create pages
+
+current_page = st.Page("1_current_data.py", title="Current Data", icon=":material/add_circle:")
+charts_page = st.Page("2_charts.py", title="Charts", icon=":material/delete:")
+dashboard_page = st.Page("3_dashboard.py", title="Dashboard", icon=":material/delete:")
+
+pg = st.navigation([current_page, charts_page, dashboard_page])
+st.set_page_config(page_title="Home Dashboard", page_icon=":material/edit:")
+pg.run()
+
+
+st.title("🎈 My Home Dashboard")
+st.image("http://localhost:8501/app/static/haus.jpg")
+
+
+
